0fab65cd2e88c0e729fa6bcaac17ee45037a3ae8
KwonHalim <129161091+KwonHalim@users.noreply.github.com>
2025-05-05 09:36:26 +0900

    :recycle: refactor: [KAN-44] 엔드포인트 예외처리 추가
    localhost:8081/noti로 접속할 때 X-USER-ID가 없는 경우 예외처리가 적절하게 되지 않아서 추가

--- common/src/main/java/befly/common/interceptor/AuthInterceptor.java
+++ common/src/main/java/befly/common/interceptor/AuthInterceptor.java
@@ -2,7 +2,10 @@ package befly.common.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.io.IOException;
 
 /*
     Gateway에서 인증되어 들어온 요청에는 X-USER-ID가 있음
@@ -10,11 +13,18 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
     로그인이 필요한 서비스에 한에 적용 되어야함.
  */
+@Slf4j
 public class AuthInterceptor implements HandlerInterceptor {
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        String userId = request.getHeader("X-USER-ID");
-        request.setAttribute("userId", Long.parseLong(userId)); // ArgumentResolver에서 꺼내 쓸 수 있게 저장
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+        try {
+            String userId = request.getHeader("X-USER-ID");
+            request.setAttribute("userId", Long.parseLong(userId)); // ArgumentResolver에서 꺼내 쓸 수 있게 저장
+        } catch (NumberFormatException e) {
+            log.error("Invalid or missing X-USER-ID header");
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid or missing X-USER-ID header");
+            return false;
+        }
         return true;
     }
 }
