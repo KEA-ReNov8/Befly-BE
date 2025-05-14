1cde667d02f10245c9ccf03a312d0de80222c600
KwonHalim <129161091+KwonHalim@users.noreply.github.com>
2025-05-14 16:33:15 +0900

    :sparkle: feat: [KAN-108] 댓글 알림 관련 로직 수정

--- community/src/main/java/befly/community/CommunityApplication.java
+++ community/src/main/java/befly/community/CommunityApplication.java
@@ -2,9 +2,11 @@ package befly.community;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @EnableKafka
+@EnableJpaAuditing
 @SpringBootApplication
 public class CommunityApplication {
     public static void main(String[] args) {
--- community/src/main/java/befly/community/dto/CommentDto.java
+++ community/src/main/java/befly/community/dto/CommentDto.java
@@ -2,13 +2,17 @@ package befly.community.dto;
 
 import befly.community.dto.kafka.NotificationType;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public class CommentDto {
     Long userId; // 댓글 단 유저의 ID
     Long postId;// 게시글의 ID(PK)
     NotificationType notificationType; //좋아요인지, 해결함인지, 고민함인지 등등 (FREEPOST, LIKE, SOLVEDPOST 받는 걸로 예정)
-    Long PcommentId;
+    Long pcommentId;
     String comment;
 
 }
