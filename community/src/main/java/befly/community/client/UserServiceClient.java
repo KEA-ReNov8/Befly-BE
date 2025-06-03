251ac567b69de784dde59224e31d8c73d65ba253
KwonHalim <129161091+KwonHalim@users.noreply.github.com>
2025-06-03 13:22:40 +0900

    :recycle: refactor: [KAN-108] FeignClient 인터페이스 추가

---
+++ community/src/main/java/befly/community/client/UserServiceClient.java
@@ -0,0 +1,13 @@
+package befly.community.client;
+
+import befly.common.apiPayload.ApiResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "user-service", url = "${service.url}")
+public interface UserServiceClient {
+    @GetMapping("/getNickname/{userId}")
+    ApiResponse<String> getUserNicknameById(@PathVariable("userId") Long userId, @RequestHeader("X-USER-ID") Long X_USER_ID);
+}
